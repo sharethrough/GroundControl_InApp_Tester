70012437add0ce0952427ce379cd8c21072f35fc
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2025-10-21 13:49:12 -0400

    Initial Commit

---
+++ GroundControlTester/GroundControlTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroundControlTesterApp.swift
+//  GroundControlTester
+//
+//  Created by Shuhao Zhang on 2025-10-21.
+//
+
+import SwiftUI
+
+@main
+struct GroundControlTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
